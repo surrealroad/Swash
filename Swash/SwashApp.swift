f45bca746f5068876b956d340b0ad6fafc55deed
Jack James <sales@surrealroad.com>
2026-07-30 18:23:37 +0100

    fix: handle cold launch drag-and-drop file opening via AppDelegate and explicit LaunchServices document extension declarations

--- Swash/SwashApp.swift
+++ Swash/SwashApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func application(_ sender: NSApplication, openFiles filenames: [String]) {
+        for filename in filenames {
+            let url = URL(fileURLWithPath: filename)
+            NSDocumentController.shared.openDocument(withContentsOf: url, display: true) { _, _, _ in }
+        }
+        sender.reply(toOpenOrPrint: .success)
+    }
+}
 
 @main
 struct SwashApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         DocumentGroup(newDocument: SwashDocument()) { file in
             ContentView(document: file.$document)
